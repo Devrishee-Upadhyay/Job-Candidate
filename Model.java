65380371baf8d2b534ef75ff94356d9e3580a1ec
Devrishee-Upadhyay <34961586+Devrishee-Upadhyay@users.noreply.github.com>
2017-12-31 19:43:24 +0530

    Update Model.java

--- Model.java
+++ Model.java
@@ -3,27 +3,27 @@ public class Model
    private String name="";
    private String description="";
    private String course="";
-   private String campus="";
+   private String city="";
 
      public Model()
 	{
 	}
 
-	public Model(String name,String description,String course,String campus)
+	public Model(String name,String description,String course,String city)
 	{
 		this.name=name;
 		this.description=description;
 		this.course=course;
-		this.campus=campus;
+		this.city=city;
 	}
 	public String getName(){ return name;}
 	public String getDescription(){ return description;}
 	public String getCourse(){return course;}
-    public String getCampus(){return campus;}
+    public String getCity(){return city;}
 
     public String toString()
 	{
-		return name+"\t"+description+"\t"+course+"\t"+campus;
+		return name+"\t"+description+"\t"+course+"\t"+city;
 	}
 
 }
